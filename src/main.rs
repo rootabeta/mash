ba807325f1e28b076b59128e173ba9f52733d7b7
rootabeta <rootabeta@protonmail.com>
2024-09-19 17:14:01 -0400

    Added command processing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
 use std::fs::read_to_string;
+use std::io::{self, Write};
+use std::process::Command;
 use std::thread;
 
 /// Multithread anything
@@ -28,8 +30,10 @@ struct Args {
 }
 
 struct Job { 
-    // Command, ready to execute
-    command: Vec<String>,
+    // Command
+    command: String,
+    // Arguments, preformatted to include targets
+    arguments: Vec<String>,
     // File to write output to
     stdout_file: Option<String>,
 
@@ -47,8 +51,15 @@ fn read_lines(file: String) -> Result<Vec<String>> {
 
 // Run command, mirroring stdout to %TARGET-argv[0].stdout
 // Stdout and stderr are echoed to screen
-fn launch_command() { 
-    todo!();
+fn launch_command(job: Job) -> Result<String> { 
+    let output = Command::new(job.command)
+        .args(job.arguments)
+        .output()?;
+
+    io::stdout().write_all(&output.stdout)?;
+    let _ = io::stdout().flush();
+
+    Ok("Placeholder".to_string())
 }
 
 fn main() {
@@ -62,12 +73,25 @@ fn main() {
     let input_lines = read_lines(args.input_file).expect("Failed to read input file");
 
 
+    let mut thread_handles = Vec::new();
     // Launch threads to consume jobs
     for _ in 0..num_threads { 
-        thread::spawn(|| { 
-            launch_command(
+        thread_handles.push(
+            thread::spawn(|| { 
+                let _ = launch_command(
+                    Job { 
+                        command: "/bin/ls".to_string(),
+                        arguments: vec!["-l".to_string(), "-a".to_string(), "/home".to_string()],
+                        stdout_file: Some("test".to_string())
+                    }
 
-            );
-        });
+                );
+            })
+        );
+    }
+
+    // Await completion of all jobs
+    for handle in thread_handles { 
+        let _res = handle.join();
     }
 }
