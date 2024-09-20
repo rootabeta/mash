a35a811f2b0bc8ef867942f4c3c26201aa9725be
rootabeta <rootabeta@protonmail.com>
2024-09-20 00:54:04 -0400

    Removed debug logs and files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::{bail, Result};
 use clap::Parser;
-use std::fs::read_to_string;
+use std::fs::{File, read_to_string};
 use std::io::{self, Write};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::thread;
 
@@ -18,7 +18,7 @@ struct Args {
     output: Option<String>,
 
     /// Disable recording output to files
-    #[arg(short, long, default_value_t = true)]
+    #[arg(short, long, default_value_t = false)]
     no_record: bool,
 
     /// Number of processes to run concurrently
@@ -28,6 +28,10 @@ struct Args {
     /// Allow clobbering of filenames
     #[arg(short, long, default_value_t = false)]
     clobber: bool,
+
+    /// Custom prefix for stdout files
+    #[arg(short, long)]
+    prefix: Option<String>,
 
     /// Command and arguments, with %INPUT% in place of the IP
     #[arg(num_args(1..))]
@@ -40,11 +44,15 @@ struct Job {
     // Arguments, preformatted to include targets
     arguments: Vec<String>,
     // Filepath to write output to
-    stdout_file: Option<String>,
+    stdout_file: PathBuf,
     // Allow clobbering files
-    clobber: bool
+    clobber: bool,
+
+    // Use stdout or not
+    use_stdout: bool
 }
 
+// Read a file line by line
 fn read_lines(file: String) -> Result<Vec<String>> { 
     let mut result = Vec::new();
 
@@ -57,11 +65,16 @@ fn read_lines(file: String) -> Result<Vec<String>> {
 
 // Run command, mirroring stdout to %TARGET-argv[0].stdout
 // Stdout and stderr are echoed to screen
-fn launch_command(job: Job) -> Result<String> { 
-    
+fn launch_command(job: Job) -> Result<()> { 
     // If the output file already exists and we do not wish to clobber, 
     // then abort before running any command
-    // TODO
+    let filepath = Path::new(&job.stdout_file);
+    if job.use_stdout && !job.clobber {
+        if filepath.exists() { 
+            // Skip running command we've already run before
+            bail!("Refusing to clobber file");
+        }
+    }
 
     let output = Command::new(job.command)
         .args(job.arguments)
@@ -69,10 +82,51 @@ fn launch_command(job: Job) -> Result<String> {
 
     io::stdout().write_all(&output.stdout)?;
     io::stderr().write_all(&output.stderr)?;
+    if job.use_stdout { 
+        let _ = File::create(filepath)?.write_all(&output.stderr);
+    }
     let _ = io::stdout().flush();
 
-    Ok("Placeholder".to_string())
+    Ok(())
 }
+
+// This is the worker thread 
+fn job_worker(rx: spmc::Receiver<Job>) { 
+    while let Ok(job) = rx.try_recv() { 
+        let _ = launch_command(
+            job
+        );
+    }
+}
+
+// Loop over all args, replacing %INPUT% with the line
+fn process_args(arguments: &Vec<String>, input: &String) -> Vec<String> { 
+    let mut result = Vec::new();
+    for argument in arguments.iter().skip(1) { 
+        result.push(
+            argument.replace("%INPUT%", input.trim())
+        )
+    }
+    result
+}
+
+// Take command like nmap -sV 1.2.3.4 and return ./outdir/nmap_-sV_1.2.3.4.stdout
+fn generate_stdout_file(directory: &String, prefix: &Option<String>, command: &String, input: &String) -> PathBuf { 
+    let stripped_input = input.trim().replace(" ", "_");
+    let filename = {
+        if let Some(prefixstr) = prefix { 
+            format!("{}#{}_{}.stdout", prefixstr, command, stripped_input)
+        } else { 
+            format!("{}_{}.stdout", command, stripped_input)
+        }
+    };
+
+    let filepath = Path::new(directory).join(filename);
+
+    filepath
+}
+
+// Create a file to store stdout results in, in the specified directory
 
 fn main() {
     let args = Args::parse();
@@ -85,25 +139,32 @@ fn main() {
 
     let input_lines = read_lines(args.input_file).expect("Failed to read input file");
 
+    for line in input_lines { 
+        let job = Job { 
+            command: args.command[0].clone(),
+            arguments: process_args(&args.command, &line),
+            clobber: allow_clobber.clone(),
+            use_stdout: !args.no_record,
+            stdout_file: generate_stdout_file(&output_dir, &args.prefix, &args.command[0], &line)
+        };
+        let _ = tx.send(
+            job
+        );
+    }
+
     let mut thread_handles = Vec::new();
     // Launch threads to consume jobs
     for _ in 0..num_threads { 
+        let crx = rx.clone();
         thread_handles.push(
-            thread::spawn(|| { 
-                let _ = launch_command(
-                    Job { 
-                        command: "/bin/ls".to_string(),
-                        arguments: vec!["-l".to_string(), "-a".to_string(), "/home".to_string()],
-                        stdout_file: Some("test".to_string()),
-                        clobber: false,
-                    }
-                );
+            thread::spawn(move|| { 
+                job_worker(crx);
             })
         );
     }
 
-    // Await completion of all jobs
-    for handle in thread_handles { 
-        let _res = handle.join();
+    // While there remains any thread that is not yet finished, wait
+    while thread_handles.iter().any(|handle| {!handle.is_finished()}) { 
+        // TODO: Progress bar that counts jobs completed and gives % and ETA
     }
 }
